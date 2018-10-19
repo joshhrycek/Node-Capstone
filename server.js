0c0dc15f3bb3068e89f75a4631ec4f2957f3f114
joshhrycek <joshhrycek@gmail.com>
2018-10-18 21:37:36 -0400

    inital commit

---
+++ server.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(express.static("public"));
+
+if (require.main === module) {
+  app.listen(process.env.PORT || 8080, function() {
+    console.info(`App listening on ${this.address().port}`);
+  });
+}
+
+module.exports = app;

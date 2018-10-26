5179f1892f8d9ce8cbb62aa46c02424a24f8888c
joshhrycek <joshhrycek@gmail.com>
2018-10-25 23:57:28 -0400

    added routes file and template endpoints

---
+++ routes/characterRouter.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+const {character} = require("../models")
+
+router.get("/", (req,res) => {
+
+});
+
+router.post("/", (req,res) => {
+
+});
+
+router.put("/:id", (req,res) => {
+
+});
+
+router.delete("/:id", (req,res) => {
+
+});
--- server.js
+++ server.js
@@ -3,14 +3,49 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const characterRouter = require(/routes/characterRouter)
 const app = express();
 
 app.use(express.static("public"));
+app.use(morgan("common"))
+
+app.use("/characters", characterRouter)
 
 if (require.main === module) {
   app.listen(process.env.PORT || 8080, function() {
     console.info(`App listening on ${this.address().port}`);
   });
 }
+function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app
+      .listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve(server);
+      })
+      .on("error", err => {
+        reject(err);
+      });
+  });
+}
 
-module.exports = app;
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log("Closing server");
+    server.close(err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer };

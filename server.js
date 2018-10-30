4089b03263f852295c852b9e7ab34cd7ff7eeeed
joshhrycek <joshhrycek@gmail.com>
2018-10-29 22:34:27 -0400

    redid

--- server.js
+++ server.js
@@ -48,4 +48,4 @@ if (require.main === module) {
   runServer().catch(err => console.error(err));
 }
 
-module.exports = { app, runServer, closeServer, server };
+module.exports = { app, runServer, closeServer};

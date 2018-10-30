d1b5f429b9018c6baefded0e7426974631b088ce
joshhrycek <joshhrycek@gmail.com>
2018-10-30 19:32:15 -0400

    I thought I fixed it

--- server.js
+++ server.js
@@ -20,13 +20,15 @@ let server ;
 function runServer(DATABASE_URL, port = PORT) {
   return new Promise((resolve, reject) => {
      
-    mongoose.connect(DATABASE_URL, err => {
+    mongoose.connect(DATABASE_URL, { useNewUrlParser: true }, err => {
        if (err) {
          return reject(err);
        };
      });
     
-    server = app
+     console.log("Found Database")
+    
+     server = app
       .listen(port, () => {
         console.log(`Your app is listening on port ${port}`);
         resolve();

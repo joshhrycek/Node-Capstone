d55e7f550749eef0c67f516fdd3076c301c8eaf2
joshhrycek <joshhrycek@gmail.com>
2018-10-30 19:49:42 -0400

    fixed port

--- config.js
+++ config.js
@@ -5,4 +5,4 @@ exports.DATABASE_URL =
 exports.TEST_DATABASE_URL = 
   process.env.TEST_DATABASE_URL || global.TEST_DATABASE_URL ||  'mongodb://localhost/NodeCapstone';
 
-exports.RAWPORT = process.env.PORT || 8080;
+exports.PORT = process.env.PORT || 8080;
